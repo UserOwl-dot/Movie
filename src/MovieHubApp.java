5f23c240aba4452637dae60db90610c3dd45db10
owl <dmitrikojevnikov@yandex.ru>
2026-02-04 16:53:37 +0300

    Добавить класс с методом main

---
+++ src/MovieHubApp.java
@@ -0,0 +1,9 @@
+import handlersandserver.MoviesServer;
+
+public class MovieHubApp {
+    public static void main(String[] args) {
+        final MoviesServer server = new MoviesServer();
+        Runtime.getRuntime().addShutdownHook(new Thread(server::stop));
+        server.start();
+    }
+}
